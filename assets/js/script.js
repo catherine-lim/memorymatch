1e7bf284db175aa4505e9e0de3573188582b9261
Catherine Chan <catherini168@gmail.com>
2019-07-08 18:10:44 -0700

    almost v3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(initializeApp);
 
+var firstCardClicked = $(event.currentTarget);
+var secondCardClicked = null;
+var matches= null;
 
 function initializeApp(){
   // $('.card').addClass(".cardback");
